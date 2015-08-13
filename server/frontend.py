1dec1f94011dc204b28ef71b6d47aab21f18a0b6
Dmitriy Kostochko <alerion.um@gmail.com>
2015-08-13 18:03:11 +0300

    update frontend server

--- server/frontend.py
+++ server/frontend.py
@@ -5,23 +5,25 @@ from os import environ
 import datetime
 
 from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
+from autobahn.wamp import register
+
+
+class TimeService(object):
+
+    @register('utcnow')
+    def utcnow(self):
+        now = datetime.datetime.utcnow()
+        return now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 class Component(ApplicationSession):
-    """
-    A simple time service application component.
-    """
 
     @asyncio.coroutine
     def onJoin(self, details):
-
-        def utcnow():
-            now = datetime.datetime.utcnow()
-            return now.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        yield from self.register(utcnow, 'utcnow')
+        yield from self.register(TimeService())
 
 
 if __name__ == '__main__':
-    runner = ApplicationRunner('ws://localhost:8080/ws', realm='frontend')
+    runner = ApplicationRunner('ws://localhost:8080/ws', realm='frontend',
+                               debug=True, debug_wamp=False, debug_app=True)
     runner.run(Component)
