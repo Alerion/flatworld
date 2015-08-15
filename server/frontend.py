dcf2e45bbb40cc374f2c0ef04fe9fddfa9043320
Dmitriy Kostochko <alerion.um@gmail.com>
2015-08-15 13:10:19 +0300

    add base of auth

--- server/frontend.py
+++ server/frontend.py
@@ -5,10 +5,42 @@ from os import environ
 import datetime
 
 from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
-from autobahn.wamp import register
+from autobahn.wamp import auth, register
+from autobahn.wamp.exception import ApplicationError
+
+
+class AuthService(object):
+
+    USERDB = {
+        'user':  {
+            # 'authid': 'ID09125',  # assign a different auth ID during authentication
+            'secret': 'user',
+            'role': 'user'
+        }
+    }
+
+    @register('authenticate')
+    def authenticate(self, realm, authid, details):
+        print("authenticate called: realm = '{}', authid = '{}'".format(realm, authid))
+
+        if authid in self.USERDB:
+            # return a dictionary with authentication information ...
+            return self.USERDB[authid]
+        else:
+            raise ApplicationError(
+                ApplicationError.AUTHORIZATION_FAILED,
+                "could not authenticate session - no such user {}".format(authid))
 
 
 class TimeService(object):
+
+    def __init__(self):
+        self.value = 1
+
+    @register('count')
+    def count(self):
+        self.value += 1
+        return self.value
 
     @register('utcnow')
     def utcnow(self):
@@ -17,13 +49,28 @@ class TimeService(object):
 
 
 class Component(ApplicationSession):
+    # See https://github.com/crossbario/crossbarexamples/blob/master/authenticate/wampcra/client.py
+    USER = "server"
+    PASSWORD = "password"
+
+    def onConnect(self):
+        self.join(self.config.realm, ["wampcra"], self.USER)
+
+    def onChallenge(self, challenge):
+        if challenge.method == "wampcra":
+            signature = auth.compute_wcs(self.PASSWORD.encode('utf8'),
+                                         challenge.extra['challenge'].encode('utf8'))
+            return signature.decode('ascii')
+        else:
+            raise Exception("don't know how to handle authmethod {}".format(challenge.method))
 
     @asyncio.coroutine
     def onJoin(self, details):
+        yield from self.register(AuthService())
         yield from self.register(TimeService())
 
 
 if __name__ == '__main__':
-    runner = ApplicationRunner('ws://localhost:8080/ws', realm='frontend',
+    runner = ApplicationRunner('ws://localhost:9000/ws', realm='frontend',
                                debug=True, debug_wamp=False, debug_app=True)
     runner.run(Component)
--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,20 +1,35 @@
+function onchallenge (session, method, extra) {
+    var KEY = 'user';
+    //console.log("onchallenge", method, extra);
+    if (method === "wampcra") {
+        console.log("onchallenge: authenticating via '" + method + "' and challenge '" + extra.challenge + "'");
+        return autobahn.auth_cra.sign(KEY, extra.challenge);
+    } else {
+        throw "don't know how to authenticate using '" + method + "'";
+    }
+}
+// TODO: add failed auth handler
 var connection = new autobahn.Connection({
-   url: 'ws://127.0.0.1:8080/ws',
-   realm: 'frontend'}
-);
+    url: 'ws://127.0.0.1:8080/ws',
+    realm: 'frontend',
+    max_retries: 3,
+    authmethods: ["wampcra"],
+    authid: "user",
+    onchallenge: onchallenge
+});
+
 
 connection.onopen = function (session) {
 
-   session.call('utcnow').then(
-      function (now) {
-         console.log("Current time:", now);
-         connection.close();
-      },
-      function (error) {
-         console.log("Call failed:", error);
-         connection.close();
-      }
-   );
+    window.call = function call(command) {
+        session.call(command).then(function(result) {
+            console.log(result);
+        });
+    };
+};
+
+connection.oncsole = function (reason, details) {
+    console.log(reason, details);
 };
 
 connection.open();
